2d6e4cb7d7d804fce69621f25826a485521e90d1
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-11-28 10:16:55 +0700

    hello

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ firebaseConfig = {
     'measurementId': "G-FQGPPP675Z"
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
-
+device = 'cVHTB2bXT3u3XSBgKeTxlq:APA91bEkPih5s4yo_eURHy33jR0myuO4IsV1sF0pvtL85pxsNRgJOcnpzq0T4abMH6-KhcVlwLB-Mufwq0o561nSudNUHKrBRvq3hdu9hl86t_A3qZqxsEIMwMUcCvc39Nvuv9vNwg43'
 db = firebase.database()
 beams_client = PushNotifications(
     instance_id='f800f676-a0af-4fee-be95-06a8d254a474',
@@ -29,7 +29,7 @@ def stream_handler(message):
     if message['data'] > 500:
         print('5 is high')
         serverToken = 'AAAAwZW1mqA:APA91bEoA3eTrSYqk_TzGfDqL3zs8NijKvRuVCXt0cFyPlc1q1Y2X2iCxktKVtVtk8BRGq7NZ03WKc-esU1jmI68fsp4JuMPRohPDm4bjq4WunQeBgdFI3M3ItLsZv7r_oSl0_KnL7cC'
-        deviceToken = 'fMJAruTgShe3lkqnsprSJy:APA91bEP97Y6TSW_wHBWyd2TPu9K2bQRd1tiir7s0vWPgaxNptMRMKqz9iFQ4baihVIlyxkzzvbTKxkUrW_Y75XrsdITYenVW1H5zNy6Q1OCz5mukuEse_pA-XEl9ZT8kSmcloHBthA-'
+        deviceToken = device
 
         headers = {
             'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ def stream_handler(message):
     if message['data'] > 500:
         print('7 is high')
         serverToken = 'AAAAwZW1mqA:APA91bEoA3eTrSYqk_TzGfDqL3zs8NijKvRuVCXt0cFyPlc1q1Y2X2iCxktKVtVtk8BRGq7NZ03WKc-esU1jmI68fsp4JuMPRohPDm4bjq4WunQeBgdFI3M3ItLsZv7r_oSl0_KnL7cC'
-        deviceToken = 'fMJAruTgShe3lkqnsprSJy:APA91bEP97Y6TSW_wHBWyd2TPu9K2bQRd1tiir7s0vWPgaxNptMRMKqz9iFQ4baihVIlyxkzzvbTKxkUrW_Y75XrsdITYenVW1H5zNy6Q1OCz5mukuEse_pA-XEl9ZT8kSmcloHBthA-'
+        deviceToken = device
 
         headers = {
             'Content-Type': 'application/json',
@@ -81,7 +81,7 @@ def stream_handler(message):
     if message['data'] > 500:
         print('135 is high')
         serverToken = 'AAAAwZW1mqA:APA91bEoA3eTrSYqk_TzGfDqL3zs8NijKvRuVCXt0cFyPlc1q1Y2X2iCxktKVtVtk8BRGq7NZ03WKc-esU1jmI68fsp4JuMPRohPDm4bjq4WunQeBgdFI3M3ItLsZv7r_oSl0_KnL7cC'
-        deviceToken = 'fMJAruTgShe3lkqnsprSJy:APA91bEP97Y6TSW_wHBWyd2TPu9K2bQRd1tiir7s0vWPgaxNptMRMKqz9iFQ4baihVIlyxkzzvbTKxkUrW_Y75XrsdITYenVW1H5zNy6Q1OCz5mukuEse_pA-XEl9ZT8kSmcloHBthA-'
+        deviceToken = device
 
         headers = {
             'Content-Type': 'application/json',
